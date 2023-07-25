c621c3b00d4d116776bc66b7845dafd67aae1d72
kshitiz-goel07 <kshitiz123goel@gmail.com>
2023-07-25 23:38:42 +0530

    Creating frontend for ToDo

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,65 @@
-import { useState } from "react"
-import { useEffect } from "react";
+// import { useState } from "react"
+// import { useEffect } from "react";
 
-function useCustomHook(){
-  const [todo, setToDo]= useState({
-    title: "gym",
-    description: "Go to gym "
-  });
+// function useCustomHook(){
+//   const [todo, setToDo]= useState({
+//     title: "gym",
+//     description: "Go to gym "
+//   });
 
-  useEffect(()=>{
-    console.log("Render");
-    setInterval(() =>{
-    setToDo({
-      title : "gym gogogogo"+ Math.random(),
-      description : "Go to gym please"
-    })
-  }, 1000)
-  }, [])
+//   useEffect(()=>{
+//     console.log("Render");
+//     setInterval(() =>{
+//     setToDo({
+//       title : "gym gogogogo"+ Math.random(),
+//       description : "Go to gym please"
+//     })
+//   }, 1000)
+//   }, [])
 
-  return todo;
-}
+//   return todo;
+// }
+
+// function App() {
+
+//   const todo = useCustomHook();
+  
+//    return (
+//     <div>
+//       <ParentHtml></ParentHtml>
+//       {todo.title}
+//       <br/>
+//       {todo.description}
+//     </div>
+//    )
+
+// }
+
+
+// function ParentHtml(){
+//   return(
+//     <div>
+//       hey
+      
+//     </div>
+//   )
+//  }
+
+
+// export default App
+
+// Above , You've implemented logics of Hooks & Custom Hooks( Practising React )
+
+// Below , This line you're creating a frontend for ToDo Application ( Will connect this frontend with a backend )
 
 function App() {
-
-  const todo = useCustomHook();
-  
    return (
     <div>
-      <ParentHtml></ParentHtml>
-      {todo.title}
-      <br/>
-      {todo.description}
+      todo application
     </div>
    )
 
 }
 
-
-function ParentHtml(){
-  return(
-    <div>
-      hey
-      
-    </div>
-  )
- }
-
-
 export default App
+
